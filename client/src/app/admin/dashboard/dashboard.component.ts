0a90f53b70f97b7d0ba8cf7703ea269f5994cb3d
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2018-04-03 17:58:13 +0100

    Add Countdown Model

--- client/src/app/admin/dashboard/dashboard.component.ts
+++ client/src/app/admin/dashboard/dashboard.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { UserService } from '../../shared';
+import { Countdown } from '../../shared/models/countdown.model';
 
 @Component({
   selector: 'admin-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class AdminDashboardComponent implements OnInit {
+
+  countdown: Countdown;
+  isSubmitting = false;
 
   constructor(
     private router: Router,
@@ -17,7 +21,7 @@ export class AdminDashboardComponent implements OnInit {
   isAuthenticated: boolean;
 
   ngOnInit() {
-
+    console.log('Admin Dshboard');
   }
 
 }
---
+++ client/src/app/shared/models/countdown.model.ts
@@ -0,0 +1,9 @@
+export interface Countdown {
+  launch_time: string,
+  title: string,
+  description: string,
+  logo: string,
+  facebook_url: string,
+  twitter_url: string,
+  behance_url: string,
+}
